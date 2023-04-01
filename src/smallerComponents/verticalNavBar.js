898c040ce243c60d06e6011aaa49b2941f4c24af
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2023-04-02 01:20:56 +0200

    vertical

--- src/smallerComponents/verticalNavBar.js
+++ src/smallerComponents/verticalNavBar.js
@@ -64,14 +64,16 @@ export const VerticalNavBar = () => {
 
   const handleLeagueClick = () => {
     setShowLeagues(!showLeagues);
+    setShowCountries(false);
   };
 
   const handleCountryClick = () => {
     setShowCountries(!showCountries);
+    setShowLeagues(false);
   };
 
   return (
-    <nav className="verticalNavBar">
+    <nav className={`verticalNavBar ${showCountries ? "expanded" : ""}`}>
       <ul>
         <li className="menuItem" onClick={handleLeagueClick}>
           <h2>Leagues</h2>
@@ -88,12 +90,8 @@ export const VerticalNavBar = () => {
           {showCountries && (
             <ul className="subMenu">
               {selectedSport.countries.map((country, index) => (
-                <div className="countries">
-                  <ul>
-                    {selectedSport.countries.map((country, index) => (
-                      <li key={index}>{country}</li>
-                    ))}
-                  </ul>
+                <div className="countriesContainer">
+                  <li key={index}>{country}</li>
                 </div>
               ))}
             </ul>
@@ -103,39 +101,3 @@ export const VerticalNavBar = () => {
     </nav>
   );
 };
-
-/* return (
-    <nav className="verticalNavBar">
-      <ul>
-        {sports.map((sport) => (
-          <li
-            key={sport.path}
-            onClick={() => handleSportClick(sport)}
-            className={selectedSport === sport ? "active" : ""}
-          >
-            {sport.icon}
-            <span>{sport.name}</span>
-          </li>
-        ))}
-        <li className="menuItem" onClick={handleLeagueClick}>
-          Leagues
-          {showLeagues && (
-            <ul className="subMenu">
-              {selectedSport.leagues.map((league, index) => (
-                <li key={index}>{league}</li>
-              ))}
-            </ul>
-          )}
-        </li>
-      </ul>
-      <div className="leagues">
-        <h3>{selectedSport.name} Leagues</h3>
-        <ul>
-          {selectedSport.leagues.map((league, index) => (
-            <li key={index}>{league}</li>
-          ))}
-        </ul>
-      </div>
-    </nav>
-  );
-}; */
