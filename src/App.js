52971f4c742b1ea616e86ac7f02881b4390bbca1
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2023-04-01 12:03:29 +0200

    initializing React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Dashboard } from "./components/Dashboard";
+import { Basketball } from "./components/Basketball";
+import { Baseball } from "./components/Baseball";
+import { Football } from "./components/Football";
+import { FavoriteTeams } from "./components/FavoriteTeams";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/football" element={<Football />} />
+        <Route path="/basketball" element={<Basketball />} />
+        <Route path="/baseball" element={<Baseball />} />
+        <Route path="/favorites" element={<FavoriteTeams />} />
+      </Routes>
     </div>
   );
 }
