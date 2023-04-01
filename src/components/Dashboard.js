857e35734b6ad6daa61f93b8d41c122857a0dce5
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2023-04-02 01:04:26 +0200

    vertical nav

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { NavBar } from "./NavBar";
+import { VerticalNavBar } from "../smallerComponents/VerticalNavBar";
 import "../styles/Dashboard.css";
 
 export const Dashboard = () => {
   return (
     <div className="container">
       <NavBar />
+      <VerticalNavBar />
     </div>
   );
 };
--- src/smallerComponents/verticalNavBar.js
+++ src/smallerComponents/verticalNavBar.js
@@ -1,31 +1,141 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { IoIosFootball, IoIosBaseball, IoIosBasketball } from "react-icons/io";
+import "../styles/VerticalNavBar.css";
 
 const sports = [
   {
     name: "Football",
     icon: <IoIosFootball />,
     leagues: ["Premier League", "La Liga", "Bundesliga", "Serie A"],
+    countries: [
+      "England",
+      "Denmark",
+      "Germany",
+      "France",
+      "Spain",
+      "Italy",
+      "Scotland",
+      "Portugal",
+    ],
+    path: "/football",
   },
   {
     name: "Baseball",
     icon: <IoIosBaseball />,
     leagues: ["MLB", "Nippon Professional Baseball", "KBO League"],
+    countries: [
+      "England",
+      "Denmark",
+      "Germany",
+      "France",
+      "Spain",
+      "Italy",
+      "Scotland",
+      "Portugal",
+    ],
+    path: "/baseball",
   },
   {
     name: "Basketball",
     icon: <IoIosBasketball />,
     leagues: ["NBA", "EuroLeague", "Basketball Bundesliga"],
+    countries: [
+      "England",
+      "Denmark",
+      "Germany",
+      "France",
+      "Spain",
+      "Italy",
+      "Scotland",
+      "Portugal",
+    ],
+    path: "/basketball",
   },
 ];
 
-export const verticalNavBar = () => {
+export const VerticalNavBar = () => {
   const [selectedSport, setSelectedSport] = useState(sports[0]);
+  const [showLeagues, setShowLeagues] = useState(false);
+  const [showCountries, setShowCountries] = useState(false);
 
   const handleSportClick = (sport) => {
     setSelectedSport(sport);
   };
 
-  return <div>verticalNavBar</div>;
+  const handleLeagueClick = () => {
+    setShowLeagues(!showLeagues);
+  };
+
+  const handleCountryClick = () => {
+    setShowCountries(!showCountries);
+  };
+
+  return (
+    <nav className="verticalNavBar">
+      <ul>
+        <li className="menuItem" onClick={handleLeagueClick}>
+          <h2>Leagues</h2>
+          {showLeagues && (
+            <ul className="subMenu">
+              {selectedSport.leagues.map((league, index) => (
+                <li key={index}>{league}</li>
+              ))}
+            </ul>
+          )}
+        </li>
+        <li className="menuItem" onClick={handleCountryClick}>
+          <h2>Countries</h2>
+          {showCountries && (
+            <ul className="subMenu">
+              {selectedSport.countries.map((country, index) => (
+                <div className="countries">
+                  <ul>
+                    {selectedSport.countries.map((country, index) => (
+                      <li key={index}>{country}</li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </ul>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
 };
+
+/* return (
+    <nav className="verticalNavBar">
+      <ul>
+        {sports.map((sport) => (
+          <li
+            key={sport.path}
+            onClick={() => handleSportClick(sport)}
+            className={selectedSport === sport ? "active" : ""}
+          >
+            {sport.icon}
+            <span>{sport.name}</span>
+          </li>
+        ))}
+        <li className="menuItem" onClick={handleLeagueClick}>
+          Leagues
+          {showLeagues && (
+            <ul className="subMenu">
+              {selectedSport.leagues.map((league, index) => (
+                <li key={index}>{league}</li>
+              ))}
+            </ul>
+          )}
+        </li>
+      </ul>
+      <div className="leagues">
+        <h3>{selectedSport.name} Leagues</h3>
+        <ul>
+          {selectedSport.leagues.map((league, index) => (
+            <li key={index}>{league}</li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}; */
